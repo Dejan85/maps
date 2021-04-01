cb7fb4bf14ecabd06bb4b5881a4722ff98d205b9
Dejan85 <xypnotica@gmail.com>
2021-04-01 02:43:39 +0200

    created api

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { User } from './src/User';
+import { Company } from './src/Company';
 
 const user = new User();
+const company = new Company();
 
 console.log('test', user);
+console.log('test', company);
